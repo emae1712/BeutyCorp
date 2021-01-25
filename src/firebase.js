46856a5a90c12623614cde3a0feeb190b92a3a5f
giovand yucra gavilan <yucragai@gmail.com>
2021-01-25 18:19:25 -0500

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+import firebase from 'firebase/app';
+var firebaseConfig = {
+    apiKey: "AIzaSyBX4TxYLSJl8urzezGI3mkRH5n6BOjhFV8",
+    authDomain: "beutycorp-a4fb3.firebaseapp.com",
+    projectId: "beutycorp-a4fb3",
+    storageBucket: "beutycorp-a4fb3.appspot.com",
+    messagingSenderId: "386504568337",
+    appId: "1:386504568337:web:12523ee1421219dfebcbb0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
