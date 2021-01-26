507fe72c63d8a58aada6f5ecfe3842ee3d226450
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-01-25 21:58:35 -0500

    Upload data, congif eslint

---
+++ src/firebase/upload-data.js
@@ -0,0 +1,22 @@
+import Data from './firebase/data-beautyCorp';
+import db from './firebase/firebase-init';
+
+const updateData = () => {
+  Data.forEach(product => {
+    db.collection('products').add({
+      id: product['Código de Producto Genérico'],
+      price: product['Precio Normal MN'],
+      brand: product.Marca,
+      imgUrl: product.Image,
+      name: product.Producto,
+      description: product.Subtipo,
+      category: product['Categoría'],
+    })
+    .then((data) => {
+      console.log(data.id);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  });
+}
