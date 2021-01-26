8f12cecb18837b90bdc3b7fa339e442e36295d2e
emae1712 <melissa.aechegaray@gmail.com>
2021-01-26 16:01:10 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // import logo from './logo.svg';
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <h1>Beuty Corp</h1>
-    </>
+      <>
+        <h1>Beuty Corp</h1>
+      </>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { NavLink } from 'react-router-dom';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import '../styles/Header.scss';
 
-const { Header } = Layout;
-
-const header = () => (
-  <Layout>
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-  </Layout>
+const Header = () => (
+  <header className="header__container">
+    <div className="logo">
+      BeautyCorp
+    </div>
+    <div className="car"><ShoppingCartOutlined /></div>
+    <nav>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: 'rgba(110, 42, 142, 1)' }} to="/lbel">
+        Lbel
+      </NavLink>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: 'rgba(110, 42, 142, 1)' }} to="/esika">
+        Esika
+      </NavLink>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: 'rgba(110, 42, 142, 1)' }} to="/cyzone">
+        Cyzone
+      </NavLink>
+    </nav>
+  </header>
 );
-export default header;
+export default Header;
