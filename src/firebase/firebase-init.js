5e2726c7e41b8acc5ad6f7e54524fedd9f207b7c
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-01-26 09:21:55 -0500

    Uploading new properties

--- src/firebase/firebase-init.js
+++ src/firebase/firebase-init.js
@@ -2,14 +2,14 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBX4TxYLSJl8urzezGI3mkRH5n6BOjhFV8",
-    authDomain: "beutycorp-a4fb3.firebaseapp.com",
-    projectId: "beutycorp-a4fb3",
-    storageBucket: "beutycorp-a4fb3.appspot.com",
-    messagingSenderId: "386504568337",
-    appId: "1:386504568337:web:12523ee1421219dfebcbb0"
-  };
+const firebaseConfig = {
+  apiKey: 'AIzaSyBX4TxYLSJl8urzezGI3mkRH5n6BOjhFV8',
+  authDomain: 'beutycorp-a4fb3.firebaseapp.com',
+  projectId: 'beutycorp-a4fb3',
+  storageBucket: 'beutycorp-a4fb3.appspot.com',
+  messagingSenderId: '386504568337',
+  appId: '1:386504568337:web:12523ee1421219dfebcbb0',
+};
 
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
--- src/firebase/upload-data.js
+++ src/firebase/upload-data.js
@@ -1,8 +1,8 @@
 import Data from './firebase/data-beautyCorp';
 import db from './firebase/firebase-init';
 
-const updateData = () => {
-  Data.forEach(product => {
+const uploadData = () => {
+  Data.forEach((product) => {
     db.collection('products').add({
       id: product['Código de Producto Genérico'],
       price: product['Precio Normal MN'],
@@ -11,12 +11,16 @@ const updateData = () => {
       name: product.Producto,
       description: product.Subtipo,
       category: product['Categoría'],
+      subcategory: product['Subcategoría'],
+      off: product['Dscto 1'],
     })
-    .then((data) => {
-      console.log(data.id);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then((data) => {
+        console.log(data.id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
-}
+};
+
+// uploadData();
