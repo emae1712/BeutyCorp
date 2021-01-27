e66469a02d373ddab15b64011c0d2d37cc86b7c7
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-01-27 13:25:17 -0500

    Trying context

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,26 @@ import {
 } from 'react-router-dom';
 import Header from './components/Header';
 import ViewProducts from './components/ViewProducts/ViewProducts';
+import CartContext from './CartContext';
+
+const data = {
+  name: 'labial',
+  description: 'labial que pinta',
+};
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/cyzone">
-            <ViewProducts />
-          </Route>
-        </Switch>
-      </Router>
+      <CartContext.Provider value={data}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/cyzone">
+              <ViewProducts />
+            </Route>
+          </Switch>
+        </Router>
+      </CartContext.Provider>
     </div>
   );
 }
